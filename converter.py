aa9d3fb3bb3f07582bc7fe309bde2e90f1d2ba13
tanguy.palmie@epitech.eu <allstart.700@gmail.com>
2021-05-24 08:40:53 +0200

    app finished

---
+++ converter.py
@@ -0,0 +1,28 @@
+# Convert webm files to mp3 using FFMPEG
+# To use this script you need to install FFMPEG in your system
+# For windows users, you need to download & add FFMPEG to the PATH (Windows Environment Variables)
+
+import os
+import re
+import subprocess
+import argparse
+import sys
+
+# The arguments
+folderConvert= "./convert"
+folderSongs="./songs"
+
+if not os.path.isdir(folderConvert):
+    print(f'The webm path "{folderConvert}" does not exist.')
+    sys.exit()
+
+
+for file in os.listdir(folderConvert):
+    webmFile = os.path.join(folderConvert, file)
+    mp3File = os.path.join(folderSongs, file).replace(
+        "webm", "mp3")
+
+    command = f"ffmpeg -i \"{webmFile}\" -vn -ab 128k -ar 44100 -y \"{mp3File}\""
+    subprocess.call(command, shell=True)
+
+    print('conversion finished')
---
+++ music.py
@@ -0,0 +1,20 @@
+import pafy
+import sys
+
+if len(sys.argv)<2:
+    link='https://www.youtube.com/watch?v=iRTuu9FPAq4'
+else: 
+    link=sys.argv[1]
+
+video = pafy.new(link)
+audiostreams = video.videostreams
+
+for i in audiostreams:
+    print('byte: %s, ext %s, size %0.2fMb' % (i.bitrate, i.extension, i.get_filesize()/1024/1024))
+
+#audiostreams[1].download(filepath = "./")
+bestaudio = video.getbestaudio()
+bestaudio.download("./convert")
+
+print("download music finished")
+import converter
